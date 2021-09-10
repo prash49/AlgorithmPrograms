0f2bb758f57b2a8cbd04f845926faeff5f48c70a
Prashanth <imprash4u@gmail.com>
2021-09-10 21:36:47 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/algorithmprograms/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.algorithmprograms;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("\n\t Welcome to Algorithm Programs");
+    }
+}
